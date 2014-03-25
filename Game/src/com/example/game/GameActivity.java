33fd03f9926e79a653795e8e6cbe09868437bbdc
Evan Murphy <esmurphy7@gmail.com>
2014-03-24 21:59:46 -0700

    worked on adding animated sprites

--- Game/src/com/example/game/GameActivity.java
+++ Game/src/com/example/game/GameActivity.java
@@ -8,6 +8,7 @@ import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.tmx.TMXLayer;
 import org.andengine.extension.tmx.TMXLoader;
@@ -19,6 +20,7 @@ import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.debug.Debug;
 
@@ -31,8 +33,9 @@ public class GameActivity extends BaseGameActivity {
 	private BoundCamera mBoundCamera;
 	private TMXTiledMap tiledMap;
 	
-	private BitmapTextureAtlas playerTexture;
-	private ITextureRegion playerTextureRegion;
+	private BitmapTextureAtlas mBitMapTextureAtlas;
+	private TiledTextureRegion playerTopHalfTextureRegion;
+	private TiledTextureRegion mHelicopterTextureRegion;
 	
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -78,14 +81,24 @@ public class GameActivity extends BaseGameActivity {
 		
 
 		/* Make the camera not exceed the bounds of the TMXEntity. */
-		this.mBoundCamera.setBounds(0, 0, tiledMap.getTMXLayers().get(0).getHeight(), tiledMap.getTMXLayers().get(0).getWidth());
+		int mapWidth = tiledMap.getTMXLayers().get(0).getWidth();
+		int mapHeight = tiledMap.getTMXLayers().get(0).getHeight();
+		this.mBoundCamera.setBounds(0, 0, mapHeight, mapWidth);
 		this.mBoundCamera.setBoundsEnabled(true);
 
 		/* Calculate the coordinates for the face, so its centered on the camera. */
-		//final float playerCenterX = (CAMERA_WIDTH - this.playerTextureRegion.getWidth()) / 2;
-		//final float playerCenterY = (CAMERA_HEIGHT - this.playerTextureRegion.getHeight()) / 2;
+		final float playerCenterX = (CAMERA_WIDTH - this.playerTopHalfTextureRegion.getWidth()) / 2;
+		final float playerCenterY = (CAMERA_HEIGHT - this.playerTopHalfTextureRegion.getHeight()) / 2;
 
-			
+		final AnimatedSprite helicopter = new AnimatedSprite(32*9, 32*2, this.mHelicopterTextureRegion, this.getVertexBufferObjectManager());
+		helicopter.animate(new long[] { 100, 100 }, 1, 2, true);
+		scene.attachChild(helicopter);
+		
+		/* Create the sprite and add it to the scene. */
+		final AnimatedSprite player = new AnimatedSprite(32*5, 32*2, 27, 28, this.playerTopHalfTextureRegion, this.getVertexBufferObjectManager());
+		this.mBoundCamera.setChaseEntity(player);
+		player.animate(100);
+		this.scene.attachChild(player);
 		
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
@@ -93,10 +106,12 @@ public class GameActivity extends BaseGameActivity {
 	private void loadGfx() {
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 		//Last two params (width and height of texture) must be to the power of 2
-		//playerTexture = new BitmapTextureAtlas(getTextureManager(), 64, 64);
+		mBitMapTextureAtlas = new BitmapTextureAtlas(getTextureManager(), 32*20, 32*10, TextureOptions.DEFAULT);
 		//Last two parameters are where the image is set to initially
-		//playerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(playerTexture, this, "player.png", 0, 0);
-		//playerTexture.load();
+		playerTopHalfTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(mBitMapTextureAtlas, this, "topwalking.png", 0, 0, 6, 1);
+		
+		mHelicopterTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitMapTextureAtlas, this, "helicopter_tiled.png", 0, 0, 2, 2);
+		mBitMapTextureAtlas.load();
 		
 	}
 	
